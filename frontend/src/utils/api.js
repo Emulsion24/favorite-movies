72683c456c0860c536c1483f3ede9d73b56a8c37
Shavandeb Kaiti <91772809+Emulsion24@users.noreply.github.com>
2025-09-06 18:23:29 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://favorite-movies-kia6.onrender.com",
+  baseURL: "https://favorite-movies-kia6.onrender.com/api/",
   withCredentials: true,
 });
 
